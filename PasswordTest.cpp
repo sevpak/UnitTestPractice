248422319905e46fa0068533c134edd0bd5f0382
Sean Evpak <sevpak@csuchico.edu>
2025-11-05 11:37:58 -0800

    Added several test cases for count_leading_characters

--- PasswordTest.cpp
+++ PasswordTest.cpp
@@ -17,6 +17,41 @@ class PasswordTest : public ::testing::Test
 TEST(PasswordTest, single_letter_password)
 {
 	Password my_password;
-	int actual = my_password.count_leading_characters("Z");
-	ASSERT_EQ(1, actual);
+	ASSERT_EQ(1, my_password.count_leading_characters("Z"));
+}
+
+TEST(PasswordTest, two_leading_letters)
+{
+	Password my_password;
+	ASSERT_EQ(2, my_password.count_leading_characters("ZZ"));
+}
+
+TEST(PasswordTest, empty_password)
+{
+	Password my_password;
+	ASSERT_EQ(0, my_password.count_leading_characters(""));
+}
+
+TEST(PasswordTest, five_leading)
+{
+	Password my_password;
+	ASSERT_EQ(5, my_password.count_leading_characters("000001"));
+}
+
+TEST(PasswordTest, repeated_characters_at_end)
+{
+	Password my_password;
+	ASSERT_EQ(1, my_password.count_leading_characters("afffffgaaaaaaeeeggsaaa"));
+}
+
+TEST(PasswordTest, special_leading_characters)
+{
+	Password my_password;
+	ASSERT_EQ(3, my_password.count_leading_characters("{{{(())}}}"));
+}
+
+TEST(PasswordTest, difference_in_capitals)
+{
+	Password my_password;
+	ASSERT_EQ(2, my_password.count_leading_characters("hhHHHHhhhHHH"));
 }
